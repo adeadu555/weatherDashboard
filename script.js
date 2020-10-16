b2fe361d721e87dbb4963bebfce0903d106c3798
adeadu555 <67295182+adeadu555@users.noreply.github.com>
2020-10-16 16:03:34 -0400

    updated Script.js

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function displayWeather () {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInAmerica + "&appid=184822765e91fe1b29c25df4c0a41998";
 
     $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+    
+        console.log(response)
 
-        
     })
 
 }
